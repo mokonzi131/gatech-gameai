a19c1929fa79c77d0faf38a6dd9a9b99d3027007
Michael Landes <mlandes3@gatech.edu>
2014-01-27 02:03:41 -0500

    Upgraded bots according to basic strategy tutorial

--- planetwars/MyBot.java
+++ planetwars/MyBot.java
@@ -12,36 +12,46 @@ public class MyBot {
     // your own. Check out the tutorials and articles on the contest website at
     // http://www.ai-contest.com/resources.
     public static void DoTurn(PlanetWars pw) {
-	// (1) If we currently have a fleet in flight, just do nothing.
-	if (pw.MyFleets().size() >= 1) {
-	    return;
-	}
-	// (2) Find my strongest planet.
-	Planet source = null;
-	double sourceScore = Double.MIN_VALUE;
-	for (Planet p : pw.MyPlanets()) {
-	    double score = (double)p.NumShips();
-	    if (score > sourceScore) {
-		sourceScore = score;
-		source = p;
-	    }
-	}
-	// (3) Find the weakest enemy or neutral planet.
-	Planet dest = null;
-	double destScore = Double.MIN_VALUE;
-	for (Planet p : pw.NotMyPlanets()) {
-	    double score = 1.0 / (1 + p.NumShips());
-	    if (score > destScore) {
-		destScore = score;
-		dest = p;
-	    }
-	}
-	// (4) Send half the ships from my strongest planet to the weakest
-	// planet that I do not own.
-	if (source != null && dest != null) {
-	    int numShips = source.NumShips() / 2;
-	    pw.IssueOrder(source, dest, numShips);
-	}
+		// (1) If we currently have two fleets in flight, just do nothing.
+    	int numFleets;
+    	boolean attackMode = false;
+    	if (pw.NumShips(1) >= pw.NumShips(2)) {
+    	    numFleets = 1;
+    	} else {
+    	    numFleets = 3;
+    	}
+    	if (pw.MyFleets().size() >= numFleets) {
+    	    return;
+    	}
+		
+		// (2) Find my strongest planet.
+		Planet source = null;
+		double sourceScore = Double.MIN_VALUE;
+		for (Planet p : pw.MyPlanets()) {
+			double score = (double)p.NumShips() / (1 + p.GrowthRate());
+		    if (score > sourceScore) {
+			sourceScore = score;
+			source = p;
+		    }
+		}
+		
+		// (3) Find the weakest enemy or neutral planet.
+		Planet dest = null;
+		double destScore = Double.MIN_VALUE;
+		for (Planet p : pw.NotMyPlanets()) {
+			double score = (double)(1 + p.GrowthRate()) / p.NumShips();
+		    if (score > destScore) {
+			destScore = score;
+			dest = p;
+		    }
+		}
+		
+		// (4) Send half the ships from my strongest planet to the weakest
+		// planet that I do not own.
+		if (source != null && dest != null) {
+		    int numShips = source.NumShips() / 2;
+		    pw.IssueOrder(source, dest, numShips);
+		}
     }
 
     public static void main(String[] args) {
