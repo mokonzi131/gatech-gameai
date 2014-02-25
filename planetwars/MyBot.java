b4502346fd7f4ba8daf168b7e43e8646c971d1a8
Michael Landes <mlandes3@gatech.edu>
2014-02-25 03:02:05 -0500

    memory improvement on snapshot

--- planetwars/MyBot.java
+++ planetwars/MyBot.java
@@ -1,48 +1,7 @@
 import java.util.*;
 
 public class MyBot {
-	private static void attack(PlanetWars pw) {
-		// (1) If we currently have two fleets in flight, just do nothing.
-    	int numFleets;
-    	boolean attackMode = false;
-    	if (pw.NumShips(1) >= pw.NumShips(2)) {
-    	    numFleets = 1;
-    	} else {
-    	    numFleets = 3;
-    	}
-    	if (pw.MyFleets().size() >= numFleets) {
-    	    return;
-    	}
-		
-		// (2) Find my strongest planet.
-		Planet source = null;
-		double sourceScore = Double.MIN_VALUE;
-		for (Planet p : pw.MyPlanets()) {
-			double score = (double)p.NumShips() / (1 + p.GrowthRate());
-		    if (score > sourceScore) {
-			sourceScore = score;
-			source = p;
-		    }
-		}
-		
-		// (3) Find the weakest enemy or neutral planet.
-		Planet dest = null;
-		double destScore = Double.MIN_VALUE;
-		for (Planet p : pw.NotMyPlanets()) {
-			double score = (double)(1 + p.GrowthRate()) / p.NumShips();
-		    if (score > destScore) {
-			destScore = score;
-			dest = p;
-		    }
-		}
-		
-		// (4) Send half the ships from my strongest planet to the weakest
-		// planet that I do not own.
-		if (source != null && dest != null) {
-		    int numShips = source.NumShips() / 2;
-		    pw.IssueOrder(source, dest, numShips);
-		}
-	}
+	private static final int LOOKAHEAD = 30;
 	
 	public static class Investment {
 		public int source;
@@ -68,12 +27,12 @@ public class MyBot {
 				int sid = source.PlanetID();
 				int did = destination.PlanetID();
 				int distance = pw.Distance(sid, did);
-				//Snapshot snapshot = node.timeline.get(distance);
+//				Snapshot snapshot = node.timeline.get(distance);
 				Snapshot snapshot = node.timeline.get(0);
 				
 				// make sure we have enough forces to invest
-				int sacrifice = snapshot.planets[did] + 1; // we need at least 1 more than they will have
-				if (snapshot.planets[sid] < sacrifice) continue;
+				int sacrifice = snapshot.data[0][did] + 1; // we need at least 1 more than they will have
+				if (snapshot.data[0][sid] < sacrifice) continue;
 				
 				// compute time it will take to net zero on investment
 				//  using makeup time by solving equation: 1 + [growth_rate][time] = [sacrifice]
@@ -97,23 +56,19 @@ public class MyBot {
 	}
 	
 	public static class Snapshot {
-		public int[] planets;
-		public int[] ownership;
+		public static final int DATASETS = 2;
+		public static final int MAX_PLANETS = 31;
 		
-		public Snapshot(int size) {
-			planets = new int[size];
-			ownership = new int[size];
-		}
+		// data[0] = planets
+		// data[1] = ownership
+		public int[][] data = new int[DATASETS][MAX_PLANETS];
 		
+		public Snapshot() {}
+				
 		private Snapshot(Snapshot other) {
-			int size = other.planets.length;
-			planets = new int[size];
-			ownership = new int[size];
-			
-			for (int i = 0; i < size; ++i) {
-				planets[i] = other.planets[i];
-				ownership[i] = other.ownership[i];
-			}
+			for (int i = 0; i < DATASETS; ++i)
+				for (int j = 0; j < MAX_PLANETS; ++j)
+					data[i][j] = other.data[i][j];
 		}
 		
 		public Snapshot clone() {
@@ -126,6 +81,7 @@ public class MyBot {
 		public int[] growthRates;
 		public List<Snapshot> timeline;
 		
+		
 		public Node(PlanetWars pw) {
 			totalPlanets = pw.NumPlanets();
 			
@@ -136,11 +92,11 @@ public class MyBot {
 			
 			// create first snapshot in timeline representing current game state
 			timeline = new ArrayList<>();
-			Snapshot current = new Snapshot(totalPlanets);
+			Snapshot current = new Snapshot();
 			for (int i = 0; i < totalPlanets; ++i) {
 				Planet planet = pw.GetPlanet(i);
-				current.planets[i] = planet.NumShips();
-				current.ownership[i] = planet.Owner();
+				current.data[0][i] = planet.NumShips();
+				current.data[1][i] = planet.Owner();
 			}
 			timeline.add(current);
 			
@@ -152,8 +108,8 @@ public class MyBot {
 				
 				// update planets according to growth
 				for (int i = 0; i < totalPlanets; ++i) {
-					if (next.ownership[i] != 0)
-						next.planets[i] += growthRates[i];
+					if (next.data[1][i] != 0)
+						next.data[0][i] += growthRates[i];
 				}
 				
 				// advance fleets to next timestep
@@ -166,7 +122,7 @@ public class MyBot {
 					for (int j = 0; j < 3; ++j)
 						forces[i][j] = 0;
 				for (int i = 0; i < totalPlanets; ++i) {
-					forces[i][next.ownership[i]] = next.planets[i];
+					forces[i][next.data[1][i]] = next.data[0][i];
 				}
 				for (Fleet fleet : fleets) {
 					if (fleet.TurnsRemaining() == 0) {
@@ -183,9 +139,9 @@ public class MyBot {
 						if (j != maxLocation && forces[i][j] > losses)
 							losses = forces[i][j];
 					int remaining = forces[i][maxLocation] - losses;
-					next.planets[i] = remaining;
+					next.data[1][i] = remaining;
 					if (remaining > 0)
-						next.ownership[i] = maxLocation;
+						next.data[1][i] = maxLocation;
 				}
 				
 				// formulate list of remaining fleets
@@ -201,14 +157,14 @@ public class MyBot {
 				current = next;
 			}
 			
-			while (timeline.size() < 30) {
+			while (timeline.size() < LOOKAHEAD) {
 				// create a new snapshot for next timestep
 				Snapshot next = current.clone();
 				
 				// update planets according to growth
 				for (int i = 0; i < totalPlanets; ++i) {
-					if (next.ownership[i] != 0)
-						next.planets[i] += growthRates[i];
+					if (next.data[1][i] != 0)
+						next.data[0][i] += growthRates[i];
 				}
 				
 				// add snapshot and update current pointer
@@ -227,8 +183,6 @@ public class MyBot {
     	
     	// perform an investment
     	invest(pw, node, time);
-    	
-    	// TODO make further investments according to strength, advantage, or desperation
     }
 
     /* LEAVE MAIN METHOD ALONE */
