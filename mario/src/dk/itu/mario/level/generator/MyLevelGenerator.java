6a3453501fa4b7e44419b0df120e8f89c6d11aad
Michael Landes <mlandes3@gatech.edu>
2014-03-06 08:16:20 -0500

    added base mario project repo

---
+++ mario/src/dk/itu/mario/level/generator/MyLevelGenerator.java
@@ -0,0 +1,25 @@
+package dk.itu.mario.level.generator;
+
+import java.util.Random;
+
+import dk.itu.mario.MarioInterface.Constraints;
+import dk.itu.mario.MarioInterface.GamePlay;
+import dk.itu.mario.MarioInterface.LevelGenerator;
+import dk.itu.mario.MarioInterface.LevelInterface;
+import dk.itu.mario.level.CustomizedLevel;
+import dk.itu.mario.level.MyLevel;
+
+public class MyLevelGenerator extends CustomizedLevelGenerator implements LevelGenerator{
+
+	public LevelInterface generateLevel(GamePlay playerMetrics) {
+		LevelInterface level = new MyLevel(320,15,new Random().nextLong(),1,LevelInterface.TYPE_OVERGROUND,playerMetrics);
+		return level;
+	}
+
+	@Override
+	public LevelInterface generateLevel(String detailedInfo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
